77c8e7849c9e490f317b18117fa1f42980805b83
MrDhillon <harman.singh.dhillon@gmail.com>
2015-09-22 07:52:29 -0400

    review feature

---
+++ rotten_mangoes/app/controllers/reviews_controller.rb
@@ -0,0 +1,26 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.build
+    #@review = Review.new(movie_id: @movie.id)
+  end
+
+  def create
+      @movies = Movie.find(params[:movie_id])
+      @review = @movies.reviews.build(review_params)
+      @review.user_id = current_user.id
+
+      if @review.save
+        redirect_to @movie, notice: "Review created successfully"
+      else
+        render :new
+      end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:text, :rating_out_of_ten)
+  end
+end
